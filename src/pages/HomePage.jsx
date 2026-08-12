c3785dbbe8995f3271e496ad640183c0b92dd47d
Ramcharan2107 <meramcharan2107@gmail.com>
2026-08-12 11:23:12 +0530

    Fix Home component import casing

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,31 +1,46 @@
-import Navbar from "../components/home/Navbar";
-import Hero from "../components/home/Hero";
-import Categories from "../components/home/Categories";
-import WhyChooseUs from "../components/home/WhyChooseUs";
-import HowItWorks from "../components/home/HowItWorks";
-import FeaturedServices from "../components/home/FeaturedServices";
-import FeaturedTechnicians from "../components/home/FeaturedTechnicians";
-import Testimonials from "../components/home/Testimonials";
-import Statistics from "../components/home/Statistics";
-import FAQ from "../components/home/FAQ";
-import Contact from "../components/home/Contact";
-import Footer from "../components/home/Footer";
+import Navbar from "../components/Home/Navbar";
+import Hero from "../components/Home/Hero";
+import Categories from "../components/Home/Categories";
+import WhyChooseUs from "../components/Home/WhyChooseUs";
+import HowItWorks from "../components/Home/HowItWorks";
+import FeaturedServices from "../components/Home/FeaturedServices";
+import FeaturedTechnicians from "../components/Home/FeaturedTechnicians";
+import Testimonials from "../components/Home/Testimonials";
+import Statistics from "../components/Home/Statistics";
+import FAQ from "../components/Home/FAQ";
+import Contact from "../components/Home/Contact";
+import Footer from "../components/Home/Footer";
+
 
 function HomePage() {
     return (
         <>
+            <Navbar />
+
             <Hero />
+
             <Categories />
+
             <WhyChooseUs />
+
             <HowItWorks />
+
             <FeaturedServices />
+
             <FeaturedTechnicians />
+
             <Testimonials />
+
             <Statistics />
+
             <FAQ />
+
             <Contact />
+
+            <Footer />
         </>
     );
 }
+
 
 export default HomePage;
